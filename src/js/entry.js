6a4a06dcfd48032918e8c91af64e9296bda80ee1
Toni-Jan Keith Monserrat <tonijanmonserrat@gmail.com>
2016-06-19 17:29:02 +0000

    Fixed installer.sh

--- src/js/entry.js
+++ src/js/entry.js
@@ -6,11 +6,11 @@ import './../../node_modules/material-design-lite/material.js';
 import './../../node_modules/mdl-ext/lib/mdl-ext.js';
 import {createApp} from 'mantra-core';
 import {initContext} from './configs/context';
-import compLib from './modules/comp-lib';
+import main from './modules/nice';
 
 const context = initContext();
 const app = createApp(context);
 
-app.loadModule(compLib);
+app.loadModule(main);
 app.init();
 context.page();
